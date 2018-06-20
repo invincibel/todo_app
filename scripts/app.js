7942f3bbdf343f588c1a5841fc522c7ac3bfae12
invincibel <31722976+invincibel@users.noreply.github.com>
2018-06-20 22:16:06 +0530

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+      //registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  }else {
+    console.log('No service-worker on this browser');
+  }
 if(!window.indexedDB){
                 window.alert('your browser does not support this app please update');
             }else{
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+var cacheName = 'app-shell-cache-v5';
+var filesToCache = [
+
+    '/',
+    'index.html',
+    'cssfile/1.css',
+    'scripts/app.js',
+    
+];
+self.addEventListener('install',function(event){
+   console.log('sw install');
+    event.waitUntil(
+    caches.open(cacheName).then(function(cache){
+       return cache.addAll(filesToCache); 
+    }).then(function(){
+        return self.skipWaiting();
+    })
+    );
+});
+self.addEventListener('activate',function(event){
+   console.log('sw-activate'); 
+    event.waitUntil(
+     caches.keys().then(keyList=>{
+         return Promise.all(keyList.map(key=>{
+             if(key!==cacheName){
+                 return caches.delete(key);
+             }
+         }));
+     }));
+    return self.clients.claim();
+});
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
