1188a6259d76b0420f6f643b08eae441a2982638
invincibel <31722976+invincibel@users.noreply.github.com>
2018-06-20 22:45:59 +0530

    final commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,10 @@
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
 if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('service-worker.js').then(function(registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
